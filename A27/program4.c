678ffaeba470f17a81aa866a86b36fb98c26343d
Neha Desai <126406727+nehadesai1204@users.noreply.github.com>
2025-11-23 22:27:11 +0530

    Add files via upload

---
+++ A27/program4.c
@@ -0,0 +1,50 @@
+/*
+Enter the number of rows :
+3
+Enter the number of cols :
+4
+*       #       *       #
+*       #       *       #
+*       #       *       #
+*/
+#include<stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    
+    int i , j = 0;
+    
+
+    for(i = 1 ; i <= iRow ; i++)
+    {
+        for(j = 1 ; j<= iCol ; j++)
+        {
+            if(j % 2 == 0)
+            {
+                   printf("#\t",j);
+            }
+            else
+            {
+                   printf("*\t",j);
+            }
+            
+        }
+        printf("\n");
+
+    }
+}
+
+int main()
+{
+     int iValue1 = 0;
+     int iValue2 = 0;
+     printf("Enter the number of rows : \n");
+     scanf("%d", &iValue1);
+
+     printf("Enter the number of cols : \n");
+     scanf("%d", &iValue2);
+
+     Pattern(iValue1 , iValue2);
+
+     return 0;
+}
