eaffd69d246d9d037ee22f1b69a85cc374d65e50
Neha Desai <126406727+nehadesai1204@users.noreply.github.com>
2026-01-02 00:23:27 +0530

    Add files via upload

---
+++ A46/program4.c
@@ -0,0 +1,154 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node **PPNODE;
+
+void Display(PNODE first)
+{
+    while(first !=NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+       iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first , int no)
+{
+   PNODE newn = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+        newn->next = *first;
+        *first = newn;
+   }
+}
+
+
+void InsertLast(PPNODE first , int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+      temp = *first;
+      while(temp->next != NULL)  //type 2
+      {
+        temp = temp->next;
+
+      }
+      temp->next = newn;
+
+   }
+
+    
+}
+
+int CountFrequency(PNODE first, int no)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        if(first->data == no)
+        {
+            iCount++;
+        }
+        first = first->next;
+    }
+    return iCount;
+}
+
+void ReplaceNegative(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        if(temp->data < 0)
+        {
+            temp->data = 0;
+        }
+        temp = temp->next;
+    }
+}
+
+
+
+
+    
+int main()
+{
+        PNODE head = NULL;
+        int iRet = 0;
+        
+
+        InsertFirst(&head,51);
+        InsertFirst(&head,21);
+        InsertFirst(&head,11);
+
+        Display(head);
+        iRet = Count(head);
+
+        printf("Number of nodes are : %d\n",iRet);
+
+        InsertLast(&head , 101);
+        InsertLast(&head , 111);
+        InsertLast(&head , 121);
+
+        Display(head);
+        iRet = Count(head);
+
+        ReplaceNegative(&head);
+    printf("After replacing negative numbers :\n");
+    Display(head);
+
+       
+   
+    return 0;
+
+}
---
+++ A46/program5.c
@@ -0,0 +1,152 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node **PPNODE;
+
+void Display(PNODE first)
+{
+    while(first !=NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+       iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first , int no)
+{
+   PNODE newn = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+        newn->next = *first;
+        *first = newn;
+   }
+}
+
+
+void InsertLast(PPNODE first , int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+      temp = *first;
+      while(temp->next != NULL)  //type 2
+      {
+        temp = temp->next;
+
+      }
+      temp->next = newn;
+
+   }
+
+    
+}
+
+int CountFrequency(PNODE first, int no)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        if(first->data == no)
+        {
+            iCount++;
+        }
+        first = first->next;
+    }
+    return iCount;
+}
+
+
+
+void IncrementAll(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        temp->data = temp->data + 1;
+        temp = temp->next;
+    }
+}
+
+
+    
+int main()
+{
+        PNODE head = NULL;
+        int iRet = 0;
+        
+
+        InsertFirst(&head,51);
+        InsertFirst(&head,21);
+        InsertFirst(&head,11);
+
+        Display(head);
+        iRet = Count(head);
+
+        printf("Number of nodes are : %d\n",iRet);
+
+        InsertLast(&head , 101);
+        InsertLast(&head , 111);
+        InsertLast(&head , 121);
+
+        Display(head);
+        iRet = Count(head);
+
+        
+    IncrementAll(&head);
+    printf("After incrementing all elements by 1 :\n");
+    Display(head);
+
+       
+   
+    return 0;
+
+}
