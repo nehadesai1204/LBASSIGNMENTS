7574756ab3a0298bd0dad7a5d7f075f8f3f5205f
Neha Desai <126406727+nehadesai1204@users.noreply.github.com>
2026-01-02 00:22:26 +0530

    Add files via upload

---
+++ A48/program4.c
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node **PPNODE;
+
+void Display(PNODE first)
+{
+    while(first !=NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+       iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first , int no)
+{
+   PNODE newn = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+        newn->next = *first;
+        *first = newn;
+   }
+}
+
+
+void InsertLast(PPNODE first , int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+      temp = *first;
+      while(temp->next != NULL)  //type 2
+      {
+        temp = temp->next;
+
+      }
+      temp->next = newn;
+
+   }
+
+    
+}
+
+void DisplayEvenPosition(PNODE Head)
+{
+    int iPos = 1;
+
+    while(Head != NULL)
+    {
+        if(iPos % 2 == 0)
+        {
+            printf("| %d | -> ", Head->data);
+        }
+        Head = Head->next;
+        iPos++;
+    }
+    printf("NULL\n");
+}
+
+
+
+
+    
+int main()
+{
+        PNODE head = NULL;
+        int iRet = 0;
+        
+
+        InsertFirst(&head,51);
+        InsertFirst(&head,21);
+        InsertFirst(&head,11);
+
+        Display(head);
+        iRet = Count(head);
+
+        printf("Number of nodes are : %d\n",iRet);
+
+        InsertLast(&head , 101);
+        InsertLast(&head , 111);
+        InsertLast(&head , 121);
+
+        Display(head);
+        iRet = Count(head);
+
+            ReplaceEven(&head);
+ 
+    printf("Elements at even positions :\n");
+    DisplayEvenPosition(head);
+
+   
+       
+   
+    return 0;
+
+}
