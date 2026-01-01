4d98d3c99b80c0e43d35431b5903b948527f1d13
Neha Desai <126406727+nehadesai1204@users.noreply.github.com>
2026-01-02 00:24:18 +0530

    Add files via upload

---
+++ A45/program2.c
@@ -0,0 +1,159 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node **PPNODE;
+
+void Display(PNODE first)
+{
+    while(first !=NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+       iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first , int no)
+{
+   PNODE newn = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+        newn->next = *first;
+        *first = newn;
+   }
+}
+
+
+void InsertLast(PPNODE first , int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+   newn = (PNODE)malloc(sizeof(NODE));
+
+   newn->data = no;
+   newn->next = NULL;
+
+   if(*first == NULL)
+   {
+       *first = newn;
+   }
+   else
+   {
+      temp = *first;
+      while(temp->next != NULL)  //type 2
+      {
+        temp = temp->next;
+
+      }
+      temp->next = newn;
+
+   }
+
+    
+}
+
+int CountFrequency(PNODE first, int no)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        if(first->data == no)
+        {
+            iCount++;
+        }
+        first = first->next;
+    }
+    return iCount;
+}
+
+int FirstOccur(PNODE Head, int No)
+{
+    int iPos = 1;
+
+    while(Head != NULL)
+    {
+        if(Head->data == No)
+        {
+            return iPos;
+        }
+        Head = Head->next;
+        iPos++;
+    }
+    return -1;   // not found
+}
+
+
+    
+int main()
+{
+        PNODE head = NULL;
+        int iRet = 0;
+        
+
+        InsertFirst(&head,51);
+        InsertFirst(&head,21);
+        InsertFirst(&head,11);
+
+        Display(head);
+        iRet = Count(head);
+
+        printf("Number of nodes are : %d\n",iRet);
+
+        InsertLast(&head , 101);
+        InsertLast(&head , 111);
+        InsertLast(&head , 121);
+
+        Display(head);
+        iRet = Count(head);
+
+       
+    iRet = FirstOccur(head, 21);
+    if(iRet != -1)
+    {
+        printf("First occurrence of 21 at position : %d\n", iRet);
+    }
+    else
+    {
+        printf("21 not found in linked list\n");
+    }
+
+    return 0;
+
+}
